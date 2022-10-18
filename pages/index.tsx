aaab647fa555bb3c2fc0e72ce38cd2377a13fd58
Kenk <kenghin_lim@hotmail.com>
2022-10-18 11:49:39 +0000

    feat: Add Dummy Icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
+import { AppButton } from "../components/AppButton/AppButton";
+
 export default function Home() {
   return (
     <div className="container mx-auto mx">
       <div className="container mx-auto mx px-8">
         <div className="grid grid-cols-4 lg:grid-cols-6 gap-4 justify-items-center ">
-      <button className="btn btn-block flex">Hello daisyUI</button>
+          <AppButton name="Github" icon="" />
+          <AppButton name="Instagram" icon="" />
+          <AppButton name="Twitter" icon="" />
+          <AppButton name="Telegram" icon="" />
+          <AppButton name="Whatsapp" icon="" />
+          <AppButton name="Kakao" icon="" />
         </div>
       </div>
     </div>
