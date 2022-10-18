3f414c5ecd20369c753a11adebcf7fe958bafe15
Kenk <kenghin_lim@hotmail.com>
2022-10-18 11:49:12 +0000

    feat: AppButton

---
+++ components/AppButton/AppButton.tsx
@@ -0,0 +1,25 @@
+interface AppButtonDetails {
+  name: string;
+  icon: string | undefined;
+}
+
+export const AppButton: React.FC<AppButtonDetails> = (
+  props: AppButtonDetails
+) => {
+  return (
+    <div className="justify-items-center">
+      {/* App Image */}
+      <button
+        className="btn p-0 w-20 h-20 shadow-md"
+        onClick={() => console.log(props.name)}
+      >
+        <img className="rounded-lg" src={props.icon} />
+      </button>
+
+      {/* App Name */}
+      <p className="font-mono font-bold text-center text-sm uppercase">
+        {props.name}
+      </p>
+    </div>
+  );
+};
