6a593fae0814754ea0ee0aeab1b7a1fa8db64b97
Kenk <kenghin_lim@hotmail.com>
2022-10-18 12:30:43 +0000

    chore: Update Navbar Text

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
-      <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      <a className="btn btn-ghost normal-case text-xl">Apps</a>
     </div>
   );
 };
