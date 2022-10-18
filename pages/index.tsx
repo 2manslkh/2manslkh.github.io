be4573a710712c9d095c1aa626deda33c3cfb832
Kenk <kenghin_lim@hotmail.com>
2022-10-18 11:48:57 +0000

    feat: Create Grid Layout for App Screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <div className="container mx-auto mx">
+      <div className="container mx-auto mx px-8">
+        <div className="grid grid-cols-4 lg:grid-cols-6 gap-4 justify-items-center ">
       <button className="btn btn-block flex">Hello daisyUI</button>
+        </div>
+      </div>
     </div>
   );
 }
