ed55ed5d746c62a0768012f39d8a2dc1ca8b7dbe
Kenk <kenghin_lim@hotmail.com>
2022-10-18 12:30:51 +0000

    feat: Add Mock Icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,12 @@ export default function Home() {
     <div className="container mx-auto mx">
       <div className="container mx-auto mx px-8">
         <div className="grid grid-cols-4 lg:grid-cols-6 gap-4 justify-items-center ">
-          <AppButton name="Github" icon="" />
-          <AppButton name="Instagram" icon="" />
-          <AppButton name="Twitter" icon="" />
-          <AppButton name="Telegram" icon="" />
-          <AppButton name="Whatsapp" icon="" />
-          <AppButton name="Kakao" icon="" />
+          <AppButton name="Github" icon="./icons/github-icon.png" />
+          <AppButton name="Instagram" icon="./icons/instagram-icon.svg" />
+          <AppButton name="Twitter" icon="./icons/twitter-icon.svg" />
+          <AppButton name="Telegram" icon="./icons/telegram-icon.svg" />
+          <AppButton name="Whatsapp" icon="./icons/whatsapp-icon.svg" />
+          <AppButton name="Kakao" icon="./icons/kakao-icon.svg" />
         </div>
       </div>
     </div>
