fb46d4a01862feb8776256efd5fa2053a60c5cab
Kenk <kenghin_lim@hotmail.com>
2022-10-18 17:44:15 +0000

    feat: Chat Page Framework

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,13 +1,28 @@
+import { UserPost } from "../components/UserPost";
+
 export default function Chat() {
   return (
-    <div className="container mx-auto">
-      <div className="grid justify-items-stretch grid-flow-row auto-rows-max divide-y-2 divide-base-300 divide-slate-400/25">
+    <div className="container mx-auto px-8">
+      <div className="grid justify-items-stretch grid-flow-row auto-rows-max divide-base-300 divide-slate-400/25">
         {/* <div className="flex flex-col w-full border-opacity-50"> */}
-        <div className="bg-primary-300 align-center">Post 1</div>
-        <div className="bg-white-300 align-center">Post 2</div>
+        <UserPost
+          avatar="./icons/lowpoly-144x144.png"
+          address="kenk.eth"
+          message="Hello World!"
+        />
+        <UserPost
+          avatar="./icons/lowpoly-144x144.png"
+          address="kenk.eth"
+          message="Hello World!"
+        />
+        <UserPost
+          avatar="./icons/lowpoly-144x144.png"
+          address="kenk.eth"
+          message="Hello World!"
+        />
         {/* </div> */}
-        <input type="text" placeholder="Type here" className="input" />
       </div>
+      <input type="text" placeholder="Type here" className="input" />
     </div>
   );
 }
